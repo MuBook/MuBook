0cff46ef68ec1ad61af0a4fd335b48032fb000b1
princemaple <chenpaul914@gmail.com>
2013-09-06 21:37:41 +1000

    try fix: cross domain ajax

--- xbook/ajax/views.py
+++ xbook/ajax/views.py
@@ -10,6 +10,9 @@ class Ajax(HttpResponse):
 	def __init__(self, *args, **kwargs):
 		super(Ajax, self).__init__(*args, **kwargs)
 		self['Access-Control-Allow-Origin'] = '*'
+		self["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+		self["Access-Control-Max-Age"] = "1000"
+		self["Access-Control-Allow-Headers"] = "*"
 
 def test(request):
 	with open(os.path.join(DATA_PATH, 'testdata.json')) as f:
