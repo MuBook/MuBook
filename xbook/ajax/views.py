12804a53fd00f38139cce70e8682817f0d7c8b98
princemaple <chenpaul914@gmail.com>
2013-09-06 21:15:50 +1000

    enabled cross domain ajax

--- xbook/ajax/views.py
+++ xbook/ajax/views.py
@@ -6,6 +6,12 @@ import os.path
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_PATH, 'data')
 
+class Ajax(HttpResponse):
+	def __init__(self, *args, **kwargs):
+		super(Ajax, self).__init__(*args, **kwargs)
+		self['Access-Control-Allow-Origin'] = '*'
+
 def test(request):
 	with open(os.path.join(DATA_PATH, 'testdata.json')) as f:
-		return HttpResponse(f.read(), mimetype='Application/json')
+		resp = Ajax(f.read(), content_type='application/json')
+		return resp
