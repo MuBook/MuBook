7d358d4bf6e7bb7f8a635b8c8dbd146690e52834
princemaple <chenpaul914@gmail.com>
2013-10-31 23:55:24 +1100

    testing using real server program to do gzip

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn xbook.wsgi
+web: gunicorn_django -b 0.0.0.0:\$PORT -w 9 -k gevent --max-requests 250 --preload src/$PYTHON_APP_NAME/settings.py
--- xbook/settings.py
+++ xbook/settings.py
@@ -121,7 +121,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.gzip.GZipMiddleware',
+    # 'django.middleware.gzip.GZipMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
