3162c8848a692c05da94c193b648be5c4bc347bd
Po Chen <pchen@zendesk.com>
2014-01-13 21:06:44 +1100

    delete unused testing code

--- xbook/ajax/urls.py
+++ xbook/ajax/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('xbook.ajax',
-	url(r'^test', 'views.ajaxJSON'),
 	url(r'^(?P<json>.*?\.json)$', 'views.ajaxJSON'),
 
 	url(r'^u-(?P<uni>.*?)/prereq/(?P<code>.*?)$', 'views.subject'),
