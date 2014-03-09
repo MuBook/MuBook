400cd8c18a775bd63f087d1a3c0b110baf41fdf1
Po Chen <pchen@zendesk.com>
2014-03-09 13:34:27 +1100

    Maybe fixed it

--- xbook/urls.py
+++ xbook/urls.py
@@ -5,17 +5,19 @@ from django.contrib import admin
 from django.contrib.auth.views import logout
 admin.autodiscover()
 
-urlpatterns = patterns('xbook.front.views',
+urlpatterns = patterns('',
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/profile/', 'user_profile'),
+)
+
+urlpatterns += patterns('xbook.front.views',
     # Examples:
     # url(r'^$', 'xbook.views.home', name='home'),
     # url(r'^xbook/', include('xbook.foo.urls')),
     url(r'^$', 'index'),
-
-    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^accounts/profile/', 'user_profile'),
-
     url(r'^ajax/', include('xbook.ajax.urls')),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/?', include(admin.site.urls)),
 
