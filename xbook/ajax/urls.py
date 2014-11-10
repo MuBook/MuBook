7c3d77dce6ee98c2b1542cf3805067a59766f475
Po Chen <pchen@zendesk.com>
2014-11-10 23:38:12 +1100

    Indentation

--- xbook/ajax/urls.py
+++ xbook/ajax/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from xbook.ajax import views
 
 urlpatterns = [
-	url(r"^u-(?P<uni>.*?)/prereq/(?P<code>.*?)$", views.subject),
-	url(r"^u-(?P<uni>.*?)/postreq/(?P<code>.*?)$", views.subject, { "prereq": False }),
+    url(r"^u-(?P<uni>.*?)/prereq/(?P<code>.*?)$", views.subject),
+    url(r"^u-(?P<uni>.*?)/postreq/(?P<code>.*?)$", views.subject, { "prereq": False }),
 
-	url(r"^u-(?P<uni>.*?)/subjectlist/?$", views.subjectList),
+    url(r"^u-(?P<uni>.*?)/subjectlist/?$", views.subjectList),
 
-  url(r"^profile/(?P<username>.*?)/?$", views.get_user_subject)
+    url(r"^profile/(?P<username>.*?)/?$", views.get_user_subject)
 ]
