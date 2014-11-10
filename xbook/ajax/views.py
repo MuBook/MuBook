39581ddfed8e67005a9b0cf824560f22eb3af904
Po Chen <pchen@zendesk.com>
2014-11-10 21:32:41 +1100

    Quotes consistency

--- xbook/ajax/views.py
+++ xbook/ajax/views.py
@@ -11,22 +11,22 @@ from xbook.ajax.models import Subject, SubjectPrereq
 from xbook.front.models import UserSubject
 from facepy import GraphAPI
 
-BOOKMARKED = 'Bookmarked'
-COMPLETED = 'Completed'
-STUDYING = 'Studying'
-PLANNED = 'Planned'
+BOOKMARKED = "Bookmarked"
+COMPLETED = "Completed"
+STUDYING = "Studying"
+PLANNED = "Planned"
 
 
 def Ajax(*args, **kwargs):
     resp = HttpResponse(*args, **kwargs)
-    resp['Access-Control-Allow-Origin'] = '*'
+    resp["Access-Control-Allow-Origin"] = "*"
     resp["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
     resp["Access-Control-Max-Age"] = "1000"
     resp["Access-Control-Allow-Headers"] = "*"
     return resp
 
 
-# return uid of users who mark 'subject' as 'state'
+# return uid of users who mark "subject" as "state"
 def subject_state_uids(subj, state):
     return map((lambda x: x.user.socialaccount_set.all()[0].uid),
                filter((lambda x: x.user.socialaccount_set.count() > 0),
@@ -39,16 +39,16 @@ def get_friends_info(local_friends_uids, subj, state):
         if uid in subject_state_uids(subj, state):
             social_account = SocialAccount.objects.filter(uid=uid)[0]
             user = social_account.user
-            friends_info.append({'fullname': user.get_full_name(),
-                                 'avatar_url': social_account.get_avatar_url(),
-                                 'fb_url': social_account.get_profile_url(),
-                                 'username': user.username})
+            friends_info.append({"fullname": user.get_full_name(),
+                                 "avatar_url": social_account.get_avatar_url(),
+                                 "fb_url": social_account.get_profile_url(),
+                                 "username": user.username})
     return friends_info
 
 
 def social_statistics(friends, subj):
-    friends_uids = map((lambda x: x.get('id')), friends)
-    local_facebook_uids = map((lambda x: x.uid), SocialAccount.objects.filter(provider='facebook'))
+    friends_uids = map((lambda x: x.get("id")), friends)
+    local_facebook_uids = map((lambda x: x.uid), SocialAccount.objects.filter(provider="facebook"))
     local_friends_uids = filter(lambda x: x in friends_uids, local_facebook_uids)
 
     friends_info_planned = get_friends_info(local_friends_uids, subj, PLANNED)
@@ -56,10 +56,10 @@ def social_statistics(friends, subj):
     friends_info_completed = get_friends_info(local_friends_uids, subj, COMPLETED)
     friends_info_bookmarked = get_friends_info(local_friends_uids, subj, BOOKMARKED)
 
-    return {'friendsInfoPlanned': friends_info_planned,
-            'friendsInfoStudying': friends_info_studying,
-            'friendsInfoCompleted': friends_info_completed,
-            'friendsInfoBookmarked': friends_info_bookmarked}
+    return {"friendsInfoPlanned": friends_info_planned,
+            "friendsInfoStudying": friends_info_studying,
+            "friendsInfoCompleted": friends_info_completed,
+            "friendsInfoBookmarked": friends_info_bookmarked}
 
 
 def attach_social_statistics(friends, nodeinfo, subj):
@@ -70,10 +70,10 @@ def attach_social_statistics(friends, nodeinfo, subj):
 def get_friends(user):
     friends = None
     if user.is_authenticated():
-        tokens = SocialToken.objects.filter(account__user=user, account__provider='facebook')
+        tokens = SocialToken.objects.filter(account__user=user, account__provider="facebook")
         if tokens:
             fb_graph = GraphAPI(tokens[0])
-            friends = fb_graph.get('me/friends').get('data')
+            friends = fb_graph.get("me/friends").get("data")
     return friends
 
 
@@ -111,8 +111,8 @@ def subject_graph(user, code, prereq=True):
         (lambda relation: relation.prereq) or \
         (lambda relation: relation.subject)
 
-    nodes = graph['nodes']
-    links = graph['links']
+    nodes = graph["nodes"]
+    links = graph["links"]
 
     friends = get_friends(user)
     parentIndex, codeToIndex = -1, { subject.code: 0 }
@@ -163,16 +163,16 @@ def subject(request, uni, code, pretty=False, prereq=True):
 
     return Ajax(
         ajaxCallback(request, info),
-        content_type='application/json'
+        content_type="application/json"
     )
 
 
 def subjectListCollector(uni, pretty=False):
-    d = {'subjList': []}
-    l = d['subjList']
+    d = {"subjList": []}
+    l = d["subjList"]
 
     for subj in Subject.objects.all():
-        this = {'code': subj.code, 'name': subj.name}
+        this = {"code": subj.code, "name": subj.name}
         l.append(this)
 
     return json.dumps(d, indent=4 if pretty else None)
@@ -182,13 +182,13 @@ def subjectListCollector(uni, pretty=False):
 def subjectList(request, uni, pretty=False):
     return Ajax(
         ajaxCallback(request, subjectListCollector(uni, pretty)),
-        content_type='application/json'
+        content_type="application/json"
     )
 
 
 def ajaxCallback(request, info):
     if "callback" in request.GET:
-        return request.GET['callback'] + "({})".format(info)
+        return request.GET["callback"] + "({})".format(info)
     else:
         return info
 
@@ -208,7 +208,7 @@ def attach_statistics(nodeinfo, subj):
 def attach_userinfo_node(user):
     def name_or_account():
         if user.first_name and user.last_name:
-            return user.first_name + ' ' + user.last_name
+            return user.first_name + " " + user.last_name
         elif user.first_name:
             return user.first_name
         elif user.last_name:
@@ -237,10 +237,10 @@ def attach_userinfo_node(user):
         "numStudying": 0,
         "numCompleted": 0,
         "numBookmarked": 0,
-        'friendsInfoPlanned': 0,
-        'friendsInfoStudying': 0,
-        'friendsInfoCompleted': 0,
-        'friendsInfoBookmarked': 0
+        "friendsInfoPlanned": 0,
+        "friendsInfoStudying": 0,
+        "friendsInfoCompleted": 0,
+        "friendsInfoBookmarked": 0
     }
 
 
@@ -250,7 +250,7 @@ def get_user_subject(request, username, pretty=False):
     if not len(User.objects.filter(username=username)):
         return Ajax(
             ajaxCallback(request, json.dumps(graph)),
-            content_type='application/json'
+            content_type="application/json"
         )
 
     selected_user = User.objects.filter(username=username)[0]
@@ -258,8 +258,8 @@ def get_user_subject(request, username, pretty=False):
 
     add_link = lambda source, target: {"source": source, "target": target}
 
-    nodes = graph['nodes']
-    links = graph['links']
+    nodes = graph["nodes"]
+    links = graph["links"]
 
     friends = get_friends(selected_user)
     parent_index = -1
@@ -303,5 +303,5 @@ def get_user_subject(request, username, pretty=False):
 
     return Ajax(
         ajaxCallback(request, info),
-        content_type='application/json'
+        content_type="application/json"
     )
