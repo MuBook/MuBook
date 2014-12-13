0cfaa74001e9ea4836bf3b4295dd8fc7abac4aae
Po Chen <pchen@zendesk.com>
2014-12-14 00:53:54 +1100

    Add notification system

--- xbook/statics/script/main.js
+++ xbook/statics/script/main.js
@@ -207,6 +207,67 @@ mubook.factory("PopupControl", ["$rootScope", "$timeout", function($rootScope, $
   return controller;
 }]);
 
+mubook.constant("NotifyDefault", {
+  duration: 5000,
+  dismissable: true
+});
+
+mubook.provider("Notify", ["NotifyDefault", function(NotifyDefault) {
+  var _config = NotifyDefault;
+
+  this.config = function(config) {
+    angular.extend(_config, config);
+  };
+
+  this.$get = ["$timeout", function($timeout) {
+    var Notify = {
+      $container: function(container) {
+        _config.container = container;
+      },
+      $dismissable: _config.dismissable,
+      $notify: function(message, status) {
+        _config.container.notify(message, status);
+        $timeout(function() {
+          _config.container.notifications.shift();
+          _config.container.$digest();
+        }, _config.duration);
+      },
+      info: function(message) {
+        this.$notify(message, 'info');
+      },
+      success: function(message) {
+        this.$notify(message, 'success');
+      },
+      error: function(message) {
+        this.$notify(message, 'danger');
+      },
+      warn: function(message) {
+        this.$notify(message, 'warning');
+      }
+    };
+    return Notify;
+  }];
+}]);
+
+mubook.directive("notification", ["Notify", function(Notify) {
+  return {
+    restrict: "E",
+    scope: false,
+    templateUrl: "directives/notification.html",
+    link: function(scope, elem) {
+      Notify.$container(scope);
+
+      scope.dismissable = Notify.$dismissable;
+
+      scope.notifications = [];
+
+      scope.notify = function(message, status) {
+        scope.notifications.push({ message: message, status: status });
+      };
+    }
+  };
+}]);
+
 mubook.controller("SearchCtrl", function SearchCtrl($scope, $timeout, Subjects, Global, PopupControl) {
   var $input = $("#searchInput");
 
